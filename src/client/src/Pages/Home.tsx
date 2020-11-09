b68a65bb194b965d8ae3ab9ef7bdc1ff09024f31
Johan Guzman <jguzm022@ucr.edu>
2020-11-09 13:16:14 -0800

    Adds temporary navigtion bar

---
+++ src/client/src/Pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+    return <h1>TODO: Add a nice home page</h1>;
+}
+
+export default Home;
---
+++ src/client/src/Pages/SearchJobs.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SearchJobs() {
+    return <h1>TODO: Adds components for the search jobs page</h1>;
+}
+
+export default SearchJobs;
