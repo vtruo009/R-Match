a421ac2a26d756525025660a14f015000e8ac252
Sydney Pun <spun001@ucr.edu>
2020-11-10 13:25:12 -0800

    Added Form placement into Create Job page.

--- src/client/src/Pages/CreateJob.tsx
+++ src/client/src/Pages/CreateJob.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import JobForm from '../Components/JobForm/JobForm';
 
 function CreateJob() {
-    return <h1>TODO: Adds components for the create jobs page</h1>;
+    //return <h1>TODO: Adds components for the create jobs page</h1>;
+    return(<JobForm />);
 }
 
 export default CreateJob;
