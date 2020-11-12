6458d1c4872da4b24317df1ae7327ab6ee9dfa7c
Sydney Pun <spun001@ucr.edu>
2020-11-12 13:48:11 -0800

    Changed font and added numeric validation and checkbox feature

--- src/client/src/Pages/CreateJob.tsx
+++ src/client/src/Pages/CreateJob.tsx
@@ -1,36 +1,59 @@
 import React from 'react';
-import { Formik, Form } from "formik";
+import { Formik, Form, Field} from "formik";
 import Button from "@material-ui/core/Button";
+import * as Yup from 'yup'; 
 
 import FormikField from "../Components/JobForm/FormikField";
 import FormikSelect, { FormikSelectItem } from "../Components/JobForm/FormikSelect";
 
+
 interface FormValues {
   title: string;
-  department: string;
   hours: string; 
   major: string; 
-  standing: string; 
+  targetYears: string; 
   courses: string; 
-  expiration: string; 
+  startDate: string;
+  endDate: string; 
   description: string; 
+  status: string; 
+  type: string; 
+  minSalary: string; 
+  maxSalary: string; 
 }
 
 const initialValues: FormValues = {
   title: "",
-  department: "",
   hours: "",
   major: "",
-  standing: "",
+  targetYears: "",
   courses: "",
-  expiration: "",
-  description: ""
+  startDate: "",
+  endDate: "",
+  description: "", 
+  status: "",
+  type: "",
+  minSalary: "",
+  maxSalary: ""
 };
 
+// Department Selection 
 const DepartmentItems: FormikSelectItem[] = [
+  { 
+    label: "Biology",
+    value: "biology"
+  },
+  {
+    label: "Biochemistry",
+    value: "biochemistry"
+  },
   {
     label: "Bioengineering",
     value: "bioengineering"
+  },
+  {
+    label: "Chemistry",
+    value: "chemistry"
   },
   {
     label: "Chemical Engineering",
@@ -56,6 +79,10 @@ const DepartmentItems: FormikSelectItem[] = [
       label: "Electrical Engineering",
       value: "electrical_engineering"
   },
+  {
+    label: "Mathematics",
+    value: "mathematics"
+  },
   { 
       label: "Materials Science and Engineering",
       value: "materials_sci_engineering"
@@ -63,8 +90,26 @@ const DepartmentItems: FormikSelectItem[] = [
   {
       label: "Mechanical Engineering",
       value: "mechanical_engineering"
+  },
+  {
+    label: "Physics",
+    value: "physics"
+  },
+  {
+    label: "Statistics",
+    value: "statistics"
   }
 ];
+
+// Regex for numeric checker
+const numericRegex = /^\d+$/; 
+
+// Input Validation
+const SignupSchema = Yup.object().shape({
+  hoursPerWeek: Yup.string()
+  .matches(numericRegex, 'All numerics required!')
+  .required('Required!')
+}) 
 
 const CreateJob: React.FC = () => {
   const handleSubmit = (values: FormValues): void => {
@@ -77,10 +122,31 @@ const CreateJob: React.FC = () => {
       <Formik
         initialValues={initialValues}
         onSubmit={handleSubmit}
+        validationSchema={SignupSchema}
       >
         {({ dirty, isValid }) => {
           return (
             <Form>
+              <div id="checkbox-group">Target Years</div>
+            <div role="group" aria-labelledby="checkbox-group">
+              <label>
+                <Field type="checkbox" name="targetYears" value="freshman" />
+                Freshman
+              </label>
+              <label>
+                <Field type="checkbox" name="targetYears" value="sophomore" />
+                Sophomore
+              </label>
+              <label>
+                <Field type="checkbox" name="targetYears" value="junior" />
+                Junior
+              </label>
+              <label>
+                <Field type="checkbox" name="targetYears" value="senior" />
+                Senior
+              </label>
+            </div>
+            <FormikField name="hoursPerWeek" label="Hours Per Week" required />
               <FormikField name="title" label="Title" required />
               <FormikSelect
                 name="department"
@@ -88,12 +154,13 @@ const CreateJob: React.FC = () => {
                 label="Department"
                 required
               />
-              <FormikField name="hoursPerWeek" label="Hours Per Week" required />
-              <FormikField name="major" label="Major" required />
-              <FormikField name="standing" label="Standing" required />
-              <FormikField name="courses" label="Courses" required />
-              <FormikField name="expiration" label="Expiration" required />
+              <FormikField name ="type" label="Type" required/>
+              <FormikField name ="startDate" label="Start Date" required/>
+              <FormikField name ="endDate" label="End Date"required/>
               <FormikField name="description" label="Description" required />
+              <FormikField name="status" label="Status" required/>
+              <FormikField name="minSalary" label="Minimum Salary" required/>
+              <FormikField name="maxSalary" label="Maximum Salary" />
               <Button
                 variant="contained"
                 color="primary"
