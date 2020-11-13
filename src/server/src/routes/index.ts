15dc79dfcc45590bea74944497813dba18f340cd
Mari-Hayashi <mhaya010@ucr.edu>
2020-11-12 16:43:19 -0800

    Finalized create job API.

--- src/server/src/routes/index.ts
+++ src/server/src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import sampleRouter from './sample';
-import jobRouter from './job';;
+import jobRouter from './job';
 
 // Init router and path
 const router = Router();
