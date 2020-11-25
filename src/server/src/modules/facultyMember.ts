3ec4b5ef5c13e337311322d7f0e0175a175080be
Mari-Hayashi <mhaya010@ucr.edu>
2020-11-25 12:49:35 -0800

    Implemented faculty member create function

--- src/server/src/modules/facultyMember.ts
+++ src/server/src/modules/facultyMember.ts
@@ -14,3 +14,43 @@ export const createFacultyMember = (user: IUser) => {
     });
     return facultyMemberRepository.save(facultyMemberToInsert);
 };
+
+/**
+ * @description saves a new faculty member profile in the database
+ * @param email string
+ * @param biography string
+ * @param firstName string
+ * @param middleName string
+ * @param lastName string
+ * @param departmentId string
+ * @param websiteLink string
+ * @param office string
+ * @param title string
+ * @returns Promise
+ */
+export const updateFacultyMember = (
+    email: IFacultyMember['email'],
+    biography: IFacultyMember['biography'],
+    firstName: IFacultyMember['firstName'],
+    middleName: IFacultyMember['middleName'],
+    lastName: IFacultyMember['lastName'],
+    departmentId: IFacultyMember['departmentId'],
+    websiteLink: IFacultyMember['websiteLink'],
+    office: IFacultyMember['office'],
+    title: IFacultyMember['title'],
+) => {
+
+    const repository = getRepository(FacultyMember);
+    const facultyMemberToInsert = repository.create({
+        email,
+        biography,
+        firstName,
+        middleName,
+        lastName,
+        departmentId,
+        websiteLink,
+        office,
+        title
+    });
+    return repository.save(facultyMemberToInsert);
+};
