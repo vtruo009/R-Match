882158e0ab75dc32914040257f46c48788e5ddb9
Sydney Pun <spun001@ucr.edu>
2020-11-26 11:36:30 -0800

    Fixed API file

--- src/client/src/Domains/FacultyMember/api/api.ts
+++ src/client/src/Domains/FacultyMember/api/api.ts
@@ -1,17 +1,5 @@
 import API from 'api';
-
-
-export interface IFacultyMemberProfileForm {
-    firstName: string; 
-    middleName: string; 
-    lastName: string; 
-    departmentId: string;
-    websiteLink: string; 
-    office: string; 
-    title: string; 
-    email: string; 
-    biography: string;
-}
+import { IFacultyMemberProfileForm } from 'Domains/FacultyMember/FacultyMemberProfileForm'
 
 //TODO: DEFINE THIS LATER!
 export async function createFacultyMemberProfile(facultyMemberProfile: IFacultyMemberProfileForm) {
