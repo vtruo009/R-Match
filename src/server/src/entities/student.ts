868dea98b857fea4e1d3df1ad2af01009f1f0940
Johan Guzman <jguzm022@ucr.edu>
2020-11-27 00:30:47 -0800

    updates entities schemas en relations

--- src/server/src/entities/student.ts
+++ src/server/src/entities/student.ts
@@ -1,28 +1,16 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    OneToOne,
+    JoinColumn,
+} from 'typeorm';
 import { User, IUser } from './user';
 
 @Entity()
-export class Student extends User {
-    constructor(
-        id: number,
-        email: string,
-        password: string,
-        biography: string,
-        firstName: string,
-        middleName: string,
-        lastName: string,
-        departmentId: string,
-        sid: number,
-        classStanding: 'freshman' | 'sophomore' | 'junior' | 'senior'
-    ) {
-        super(id, email, password, biography, firstName, middleName, lastName);
-        this.studentId = id;
-        this.departmentId = departmentId;
-        this.sid = sid;
-        this.classStanding = classStanding;
-    }
+export class Student {
     @PrimaryGeneratedColumn()
-    studentId: number;
+    id: number;
 
     @Column({ nullable: true })
     departmentId: string;
@@ -31,12 +19,17 @@ export class Student extends User {
     sid: number;
 
     @Column({ nullable: true })
-    classStanding: string;
+    classStanding: 'freshman' | 'sophomore' | 'junior' | 'senior';
+
+    @OneToOne(() => User)
+    @JoinColumn()
+    user: User;
 }
 
-export interface IStudent extends IUser {
-    studentId: number;
+export interface IStudent {
+    id: number;
     departmentId: string;
     sid: number;
     classStanding: 'freshman' | 'sophomore' | 'junior' | 'senior';
+    user: IUser;
 }
--- src/server/src/entities/user.ts
+++ src/server/src/entities/user.ts
@@ -1,27 +1,9 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export abstract class User {
-    constructor(
-        id: number,
-        email: string,
-        password: string,
-        biography: string,
-        firstName: string,
-        middleName: string,
-        lastName: string
-    ) {
-        this.userId = id;
-        this.email = email;
-        this.password = password;
-        this.biography = biography;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.middleName = middleName;
-    }
-
+export class User {
     @PrimaryGeneratedColumn()
-    userId: number;
+    id: number;
 
     @Column()
     email: string;
@@ -40,14 +22,18 @@ export abstract class User {
 
     @Column({ nullable: true })
     middleName: string;
+
+    @Column()
+    role: 'student' | 'facultyMember';
 }
 
 export interface IUser {
-    userId: number;
+    id: number;
     email: string;
     password: string;
-    biography: string;
+    biography?: string;
     firstName: string;
     middleName?: string;
     lastName: string;
+    role: 'student' | 'facultyMember';
 }
