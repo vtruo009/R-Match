ce37d8b79698faf78c1358407c7bbc1f71d38b48
Johan Guzman <jguzm022@ucr.edu>
2020-11-27 00:30:18 -0800

    finishes helper modules

--- src/server/src/modules/user.ts
+++ src/server/src/modules/user.ts
@@ -18,13 +18,15 @@ export const findUserByEmail = (email: string) => {
  * @param password user's password *
  * @param firstName user's first name
  * @param lastName user's last name
+ * @param role user's role, either 'student' or 'facultyMember'
  * @returns Promise
  */
 export const createUser = (
     email: IUser['email'],
     password: IUser['password'],
     firstName: IUser['firstName'],
-    lastName: IUser['lastName']
+    lastName: IUser['lastName'],
+    role: IUser['role']
 ) => {
     const repository = getRepository(User);
     const userToInsert = repository.create({
@@ -32,32 +34,44 @@ export const createUser = (
         password,
         firstName,
         lastName,
+        role,
     });
     return repository.save(userToInsert);
 };
 
+/**
+ * @description registers user's information
+ * @param email user's email address
+ * @param password user's password (not encrypted)
+ * @param firstName user's first name
+ * @param lastName user's last name
+ * @param role user's role, either 'student' or 'facultyMember'
+ * @returns Promise
+ */
 export const registerUser = async (
     email: IUser['email'],
     password: IUser['password'],
     firstName: IUser['firstName'],
     lastName: IUser['lastName'],
-    role: 'student' | 'facultyMember'
+    role: IUser['role']
 ) => {
     const encryptedPassword = await bcrypt.hash(password, 10);
+    // Create user regardless of role
+    const newUser = await createUser(
+        email,
+        encryptedPassword,
+        firstName,
+        lastName,
+        role
+    );
     switch (role) {
         case 'student':
-            // Inserts information in Student table
-            return createStudent(email, encryptedPassword, firstName, lastName);
+            // Inserts information in student table
+            return createStudent(newUser);
         case 'facultyMember':
             // Inserts information in facultyMember table
-            return createFacultyMember(
-                email,
-                encryptedPassword,
-                firstName,
-                lastName
-            );
+            return createFacultyMember(newUser);
         default:
-            // Inserts information in user table?
-            return createUser(email, encryptedPassword, firstName, lastName);
+            return undefined;
     }
 };
