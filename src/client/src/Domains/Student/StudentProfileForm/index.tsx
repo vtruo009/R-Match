6444b261fcd9be090c6825afac2e6426fd03ffc4
Sydney Pun <spun001@ucr.edu>
2020-11-28 18:36:04 -0800

    Formatted student form

---
+++ src/client/src/Domains/Student/StudentProfileForm/index.tsx
@@ -0,0 +1,160 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { Formik, Form, Field } from 'formik';
+import * as yup from 'yup';
+
+import useApi from 'hooks/useApi';
+import useSnack from 'hooks/useSnack';
+import Loader from 'Components/Loader';
+import { TextFormField } from 'Components/TextFormField';
+import { SelectFormField } from 'Components/SelectFormField';
+import Button from 'Components/Button';
+import {
+    classStandingTypes,
+    createStudentProfile,
+} from 'Domains/Student/api/api';
+
+import {
+    departments,
+} from 'sharedData'
+
+export interface IStudentProfileForm {
+    firstName: string; 
+    middleName: string; 
+    lastName: string; 
+    departmentId: string;
+    SID: number; 
+    classStanding: string; 
+    email: string; 
+    biography: string;
+}
+
+const formInitialValues: IStudentProfileForm = {
+    firstName: '',
+    middleName: '', 
+    lastName: '', 
+    departmentId: '',
+    SID: 0, 
+    classStanding: '',
+    email: '',
+    biography: '',
+};
+
+const formSchema = yup.object({
+    firstName: yup.string().required('First name is required'),
+    lastName: yup.string().required('Last name is required'),
+    departmentId: yup.string().required('Department is required'),
+    SID: yup.string().required('Student ID is is required'),
+    classStanding: yup.string().required("Class standing is required"),
+    title: yup.string().required("Title is required"),
+    email: yup.string().required("Email is required"),
+    biography: yup.string().required("Biography is required")
+});
+
+function StudentProfileForm() {
+    const [studentProfile, setStudentProfile] = React.useState<IStudentProfileForm>(formInitialValues);
+    const request = React.useCallback(() => createStudentProfile(studentProfile), [studentProfile]);
+    const [snack] = useSnack();
+    const [sendRequest, isLoading] = useApi(request, {
+        onSuccess: () => {
+            snack('Student profile successfully created!', 'success');
+        },
+    });
+    return (
+        <Paper style={{ padding: 50 }}>
+            <Formik
+                validationSchema={formSchema}
+                initialValues={formInitialValues}
+                onSubmit={(formValues, actions) => {
+                    setStudentProfile(formValues);
+                    sendRequest();
+                    actions.resetForm({
+                        values: { ...formInitialValues },
+                    });
+                }}
+            >
+                {() => (
+                    <Form>
+                        <Grid container spacing={3} alignContent='center'>
+                            <Grid item container justify='flex-start'>
+                                <Typography variant='h4'>Create Student Profile</Typography>
+                            </Grid>
+                            <Grid item container spacing={5}>
+                                <Grid item md={4} xs={12}>
+                                    <Field
+                                        name='firstName'
+                                        label='First Name'
+                                        component={TextFormField}
+                                    />
+                                </Grid>
+                                <Grid item md={4} xs={12}>
+                                    <Field
+                                        name='middleName'
+                                        label='Middle Name'
+                                        component={TextFormField}
+                                    />
+                                </Grid>
+                                <Grid item md={4} xs={12}>
+                                    <Field
+                                        name='lastName'
+                                        label='Last Name'
+                                        component={TextFormField}
+                                    />
+                                </Grid>
+                                <Grid item md={6} xs={12}>
+                                    <Field
+                                        name='departmentId'
+                                        label='Department'
+                                        options={departments}
+                                        component={SelectFormField}
+                                    />
+                                </Grid>
+                                <Grid item md={6} xs={12}>
+                                    <Field
+                                        name='classStanding'
+                                        label='Class Standing'
+                                        options={classStandingTypes}
+                                        component={SelectFormField}
+                                    />
+                                </Grid>
+                                <Grid item md={6} xs={12}>
+                                    <Field
+                                        name='SID'
+                                        label='SID'
+                                        type = 'number'
+                                        component={TextFormField}
+                                    />
+                                </Grid>
+                                <Grid item md={6} xs={12}>
+                                    <Field
+                                        name='email'
+                                        label='Email'
+                                        component={TextFormField}
+                                    />
+                                </Grid>
+                                <Grid item md={12} xs={12}>
+                                    <Field
+                                        name='biography'
+                                        label='Biography'
+                                        multiline
+                                        component={TextFormField}
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Grid container item xs={12}>
+                                <Button type='submit' isLoading={isLoading}>
+                                    Submit
+                                    {isLoading && <Loader size={20} />}
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Form>
+                )}
+            </Formik>
+        </Paper>
+    );
+}
+
+export default StudentProfileForm;
---
+++ src/client/src/Pages/CreateStudentProfile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StudentProfileForm from 'Domains/Student/StudentProfileForm';
+
+function CreateStudentProfile() {
+    return (
+        <div style={{ margin: 50 }}>
+            <StudentProfileForm />
+        </div>
+    );
+}
+
+export default CreateStudentProfile;
