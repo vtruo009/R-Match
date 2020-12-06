eeede22ca1ec0eb04268b19f8e4ee5d169a3d8a3
Sydney Pun <spun001@ucr.edu>
2020-12-06 15:34:43 -0800

    Changed type to file

--- src/server/src/entities/student.ts
+++ src/server/src/entities/student.ts
@@ -27,10 +27,10 @@ export class Student {
     classStanding: 'freshman' | 'sophomore' | 'junior' | 'senior';
 
     @Column({ nullable: true })
-    resume: string; 
+    resume: File; 
 
     @Column({ nullable: true })
-    transcript: string; 
+    transcript: File; 
 
     @OneToOne(() => User)
     @JoinColumn()
@@ -46,8 +46,8 @@ export interface IStudent {
     department?: Department;
     sid?: number;
     classStanding?: 'freshman' | 'sophomore' | 'junior' | 'senior';
-    resume?: string; 
-    transcript?: string; 
+    resume?: File; 
+    transcript?: File; 
     user: IUser;
     courses?: Course[];
 }
