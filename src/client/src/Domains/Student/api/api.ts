8d14e29666fadbd1e0e783dccce14ab371af1fe2
Johan Guzman <jguzm022@ucr.edu>
2020-12-10 11:59:51 -0800

    Remove unneeded files

--- src/client/src/Domains/Student/api/api.ts
+++ src/client/src/Domains/Student/api/api.ts
@@ -1,6 +1,5 @@
 import API from 'api';
 import { IStudentProfileForm } from 'Domains/Student/StudentProfileForm';
-import { IUser } from 'Domains/Accounts/api/api';
 
 export const classStandingTypes = [
     {
@@ -21,33 +20,6 @@ export const classStandingTypes = [
     },
 ];
 
-// TODO: Move these interfaces to their respective in the future
-interface ICollege {
-    id: number;
-    name: string;
-}
-interface IDepartment {
-    id: number;
-    name: string;
-    college: ICollege;
-}
-
-interface ICourse {
-    id: number;
-    title: string;
-}
-
-export interface IStudent {
-    id: number;
-    user: IUser;
-    department?: IDepartment;
-    sid?: number;
-    classStanding?: string;
-    courses?: ICourse[];
-    resume?: File;
-    transcript?: File;
-}
-
 export async function updateStudentProfile(
     studentProfile: IStudentProfileForm
 ) {
@@ -56,9 +28,4 @@ export async function updateStudentProfile(
             ...studentProfile,
         },
     });
-}
-
-// No parameters needed since user will have to be authenticated
-export async function getStudentProfile() {
-    return API.get<{ student: IStudent }>('student/get-profile');
 }
