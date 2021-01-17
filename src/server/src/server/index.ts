c96e515c986dd9524088c1580fcf284384acc132
Johan Guzman <jguzm022@ucr.edu>
2021-01-16 23:44:44 -0800

    Sets up env variables for testing environment

--- src/server/src/server/index.ts
+++ src/server/src/server/index.ts
@@ -1,6 +1,12 @@
-import './preStart';
+import '@dotenv/config';
+import { connectToDb } from '@db/connection';
 import app from 'src/app';
 import logger from '@shared/Logger';
+
+// Initializes database
+(async () => {
+    await connectToDb();
+})();
 
 // Start the server
 const port = Number(process.env.PORT || 5000);
