45a02f58b48a5947ceb7cee53a23a3a809fb7b88
Mari-Hayashi <mhaya010@ucr.edu>
2021-02-04 21:16:08 -0800

    Removed unnecessary spaces.

--- src/server/src/modules/facultyMember.ts
+++ src/server/src/modules/facultyMember.ts
@@ -98,7 +98,7 @@ export const getPostedJobs = async (
     // Check if a faculty member with the given id exists.
     const facultyMember = await FacultyMember.findOne(facultyMemberId);
     if (!facultyMember) return undefined;
-    
+
     // Returns all jobs he posted.
     return getRepository(Job)
         .createQueryBuilder('job')
