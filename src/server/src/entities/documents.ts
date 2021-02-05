1995f9350d765a1c260c047088bb8b25d0799bea
Van Truong <vantruong@Vans-MacBook-Pro.local>
2021-02-04 20:12:34 -0800

    Set up the Document table in entities

---
+++ src/server/src/entities/documents.ts
@@ -0,0 +1,29 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    ManyToOne,
+    BaseEntity,
+} from 'typeorm';
+import { Student } from '../entities/student';
+
+@Entity()
+export class Document extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ nullable: false })
+    name: string;
+
+    @Column({ nullable: true })
+    isDefault: boolean;
+
+    @Column({ nullable: false})
+    dateAdded: Date;
+
+    @Column({ type: 'bytea', nullable: false })
+    document: Buffer;
+
+    @ManyToOne(() => Student, (student) => (student.documents))
+    public student: Student;
+}
